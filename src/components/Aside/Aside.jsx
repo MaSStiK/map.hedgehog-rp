9bb940ce39ac1fce881883718313958967813b1b
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-06-20 16:40:41 +0200

    open main site

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ButtonProfile from "../ButtonProfile/ButtonProfile"
 import ButtonImage from "../ButtonImage/ButtonImage"
 import Selection from "./Selection"
@@ -8,13 +9,16 @@ import "./Aside.css"
 import "./Aside-phone.css"
 
 export default function Aside() {
+    const Navigate = useNavigate()
+
     return (
         <aside className="flex-col">
             <ButtonProfile
                 src={imgLogo}
                 text={"Основной сайт"}
+                onClick={() => window.open("https://hedgehog-rp.ru", "_self")}
             />
-            
+
             <Selection />
         </aside>
     )
