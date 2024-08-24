c2b7dbcdedd6784c1f9b174be07c7b372b3a9e69
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-08-24 21:32:05 +0200

    new paths

--- src/components/MapPage/ActualMap.js
+++
@@ -1,6 +0,0 @@
-// import imgWorldMap from "../../assets/maps/s4/19.06.2024.png"
-import MapOptions from "../MapSelection/MapSelection.jsx"
-
-export default function ActualMap(Context) {
-    Context.setMap({url: MapOptions.s4[0].url})
-}
--- src/components/MapSelection/MapSelection.jsx
+++ src/components/MapSelection/MapSelection.jsx
@@ -2,23 +2,6 @@ import { useState, useContext, useEffect } from "react";
 import CustomSelect from "../CustomSelect/CustomSelect"
 import { DataContext } from "../Context"
 
-
-// s4
-import imgS4_24_08_2024 from "../../assets/maps/s4/24.08.2024.png"
-import imgS4_11_08_2024 from "../../assets/maps/s4/11.08.2024.png"
-import imgS4_27_07_2024 from "../../assets/maps/s4/27.07.2024.png"
-import imgS4_19_07_2024 from "../../assets/maps/s4/19.07.2024.png"
-import imgS4_19_06_2024 from "../../assets/maps/s4/19.06.2024.png"
-import imgS4_17_06_2024 from "../../assets/maps/s4/17.06.2024.png"
-import imgS4_14_06_2024 from "../../assets/maps/s4/14.06.2024.png"
-import imgS4_12_06_2024 from "../../assets/maps/s4/12.06.2024.png"
-import imgLoreMap_S4 from "../../assets/maps/s4/lore-map.png"
-import imgPhysicalMap_S4 from "../../assets/maps/s4/physical-map.png"
-
-
-// s3
-import imgPhysicalMap_S3 from "../../assets/maps/s3/physical-map.png"
-
 const seasonOptions = [
     {value: "s4", label: "Сезон 4 (Текущий)"},
     {value: "s3", label: "Сезон 3"},
@@ -28,20 +11,20 @@ const seasonOptions = [
 
 export const MapOptions = {
     s4: [
-        {value: "24.08.2024", label: "24.08.2024", url: imgS4_24_08_2024},
-        {value: "11.08.2024", label: "11.08.2024", url: imgS4_11_08_2024},
-        {value: "27.07.2024", label: "27.07.2024", url: imgS4_27_07_2024},
-        {value: "17.07.2024", label: "17.07.2024", url: imgS4_19_07_2024},
-        {value: "19.06.2024", label: "19.06.2024", url: imgS4_19_06_2024},
-        {value: "17.06.2024", label: "17.06.2024", url: imgS4_17_06_2024},
-        {value: "14.06.2024", label: "14.06.2024", url: imgS4_14_06_2024},
-        {value: "12.06.2024", label: "12.06.2024", url: imgS4_12_06_2024},
-        {value: "lore-map", label: "Лорная карта", url: imgLoreMap_S4},
-        {value: "physical_map", label: "Физическая карта", url: imgPhysicalMap_S4},
+        {value: "24.08.2024", label: "24.08.2024", url: require("../../assets/maps/s4/24.08.2024.png")},
+        {value: "11.08.2024", label: "11.08.2024", url: require("../../assets/maps/s4/11.08.2024.png")},
+        {value: "27.07.2024", label: "27.07.2024", url: require("../../assets/maps/s4/27.07.2024.png")},
+        {value: "19.07.2024", label: "19.07.2024", url: require("../../assets/maps/s4/19.07.2024.png")},
+        {value: "19.06.2024", label: "19.06.2024", url: require("../../assets/maps/s4/19.06.2024.png")},
+        {value: "17.06.2024", label: "17.06.2024", url: require("../../assets/maps/s4/17.06.2024.png")},
+        {value: "14.06.2024", label: "14.06.2024", url: require("../../assets/maps/s4/14.06.2024.png")},
+        {value: "12.06.2024", label: "12.06.2024", url: require("../../assets/maps/s4/12.06.2024.png")},
+        {value: "lore-map", label: "Лорная карта", url: require("../../assets/maps/s4/lore-map.png")},
+        {value: "phys_map", label: "Физическая карта", url: require("../../assets/maps/s4/physical-map.png")},
     ],
 
     s3: [
-        {value: "physical_map", label: "Физическая карта", url: imgPhysicalMap_S3},
+        {value: "phys_map", label: "Физическая карта", url: require("../../assets/maps/s3/physical-map.png")},
     ]
 }
 
