482517fea069a26ccf33288308a11bc70be1646d
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-02-23 17:35:19 +0100

    2025.02.23

--- src/components/MapSelection/MapSelection.jsx
+++ src/components/MapSelection/MapSelection.jsx
@@ -11,7 +11,8 @@ const seasonOptions = [
 
 export const MapOptions = {
     s4: [
-        {value: "03.01.202_2", label: "03.01.2025_2", url: require("../../assets/maps/s4/2025.01.03_2.png")},
+        {value: "23.02.2025", label: "23.02.2025", url: require("../../assets/maps/s4/2025.02.23.png")},
+        {value: "03.01.2025_2", label: "03.01.2025_2", url: require("../../assets/maps/s4/2025.01.03_2.png")},
         {value: "03.01.2025", label: "03.01.2025", url: require("../../assets/maps/s4/2025.01.03.png")},
         {value: "02.01.2025", label: "02.01.2025", url: require("../../assets/maps/s4/2025.01.02.png")},
         {value: "29.12.2024", label: "29.12.2024", url: require("../../assets/maps/s4/2024.12.29.png")},
