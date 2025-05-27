81a47a6ef5393d07f1dc54b34a488d1b1aa8d1cf
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-05-27 13:41:02 +0200

    aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,7 +1,5 @@
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ButtonProfile from "../ButtonProfile/ButtonProfile"
-import ButtonImage from "../ButtonImage/ButtonImage"
 import MapSelection from "../MapSelection/MapSelection"
 import imgLogo from "../../assets/logo/logoFullSize.png"
 
@@ -9,8 +7,6 @@ import "./Aside.css"
 import "./Aside-phone.css"
 
 export default function Aside() {
-    const Navigate = useNavigate()
-
     return (
         <aside className="flex-col">
             <Link to="https://hedgehog-rp.ru">
