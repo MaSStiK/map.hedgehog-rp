b0922b229ae7f2749592559c4cdf3d8d05270754
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-06-23 20:58:17 +0200

    maps

--- src/components/MapSelection/s1.js
+++ src/components/MapSelection/s1.js
@@ -7,7 +7,9 @@ const s1 = [
     { value: "2020.10.02_2",    label: "2020.10.02_2",            url: path + "2020.10.02_2.png" },
     { value: "2020.10.02",      label: "2020.10.02",              url: path + "2020.10.02.png" },
     { value: "2020.06.30",      label: "2020.06.30",              url: path + "2020.06.30.png" },
+    { value: "2021.03.25",      label: "2021.03.25 Куйбышев", url: path + "2021.03.25.png" },
     { value: "2021.03.XX",      label: "2021.03.XX Коалоежинск ", url: path + "2021.03.XX.png" },
+    { value: "2021.01.XX",      label: "2021.01.XX Афины",       url: path + "2021.01.XX.png" },
     { value: "2020.06.15",      label: "2020.06.15",              url: path + "2020.06.15.png" }
 ];
 
