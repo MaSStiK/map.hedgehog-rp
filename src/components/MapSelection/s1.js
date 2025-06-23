cc08df2bf4cacf54fa71338c82c169c086836723
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-06-23 20:56:58 +0200

    maps

--- src/components/MapSelection/s1.js
+++ src/components/MapSelection/s1.js
@@ -1,6 +1,14 @@
 const path = "/maps/s1/"
 
 const s1 = [
-]
+    { value: "physical-map",    label: "Физическая карта",        url: path + "physical-map.png" },
+    { value: "space-map",       label: "Космическая карта",       url: path + "space-map.png" },
+    { value: "2020.12.XX",      label: "2020.12.XX",              url: path + "2020.12.XX.png" },
+    { value: "2020.10.02_2",    label: "2020.10.02_2",            url: path + "2020.10.02_2.png" },
+    { value: "2020.10.02",      label: "2020.10.02",              url: path + "2020.10.02.png" },
+    { value: "2020.06.30",      label: "2020.06.30",              url: path + "2020.06.30.png" },
+    { value: "2021.03.XX",      label: "2021.03.XX Коалоежинск ", url: path + "2021.03.XX.png" },
+    { value: "2020.06.15",      label: "2020.06.15",              url: path + "2020.06.15.png" }
+];
 
 export default s1
