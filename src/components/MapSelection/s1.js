ee561f0e3d213be430cbd59dfdbb989131be279d
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-06-23 22:28:42 +0200

    changes

--- src/components/MapSelection/s1.js
+++ src/components/MapSelection/s1.js
@@ -3,9 +3,9 @@ const path = "/maps/s1/"
 const s1 = [
     { value: "physical-map",    label: "Физическая карта",               url: path + "physical-map.png" },
     { value: "space-map",       label: "Космическая карта",              url: path + "space-map.png" },
-    { value: "2021.03.25",      label: "2021.03.25 Куйбышев",            url: path + "2021.03.25.png" },
-    { value: "2021.03.XX",      label: "2021.03.XX Коалоежинск",         url: path + "2021.03.XX.png" },
-    { value: "2021.01.XX",      label: "2021.01.XX Афины",               url: path + "2021.01.XX.png" },
+    { value: "2021.03.25",      label: "2021.03.25 Великоежинск",        url: path + "2021.03.25.png" },
+    { value: "2021.03.21",      label: "2021.03.21 Коалоежинск",         url: path + "2021.03.21.png" },
+    { value: "2021.03.14",      label: "2021.03.14 Афины",               url: path + "2021.03.14.png" },
     { value: "2020.12.XX",      label: "2020.12.XX Цанг",                url: path + "2020.12.XX.png" },
     { value: "2020.11.30",      label: "2020.11.30 Лос-Анджелес",        url: path + "2020.11.30.png" },
     { value: "2020.11.15",      label: "2020.11.15 Санса",               url: path + "2020.11.15.png" },
