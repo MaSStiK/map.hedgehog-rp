a32c0fb1d16719d74abd4f74dcc6171ee40ecfe5
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-04 23:20:11 +0200

    2025.07.04

--- src/components/MapSelection/s5.js
+++ src/components/MapSelection/s5.js
@@ -1,6 +1,8 @@
 const path = "/maps/s5/"
 
 const s5 = [
+    { value: "2025.07.04_dnh", label: "2025.07.04 Динахон", url: path + "2025.07.04_dnh.png" },
+    { value: "2025.07.04",     label: "2025.07.04",         url: path + "2025.07.04.png" },
     { value: "2025.06.05_dnh", label: "2025.06.05 Динахон", url: path + "2025.06.05_dnh.png" },
     { value: "2025.05.27",     label: "2025.05.27",         url: path + "2025.05.27.png" },
     { value: "2025.05.27_dnh", label: "2025.05.27 Динахон", url: path + "2025.05.27_dnh.png" },
