ef846b07d0651bfff11d8168db3dcb65594e5cb5
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-08-09 10:57:52 +0200

    2025.08.09_klb

--- src/components/MapSelection/s5.js
+++ src/components/MapSelection/s5.js
@@ -1,16 +1,17 @@
 const path = "/maps/s5/"
 
 const s5 = [
-    { value: "2025.07.31_dnh", label: "2025.07.31 Динахон", url: path + "2025.07.31_dnh.png" },
-    { value: "2025.07.04_dnh", label: "2025.07.04 Динахон", url: path + "2025.07.04_dnh.png" },
-    { value: "2025.07.04",     label: "2025.07.04",         url: path + "2025.07.04.png" },
-    { value: "2025.06.05_dnh", label: "2025.06.05 Динахон", url: path + "2025.06.05_dnh.png" },
-    { value: "2025.05.27",     label: "2025.05.27",         url: path + "2025.05.27.png" },
-    { value: "2025.05.27_dnh", label: "2025.05.27 Динахон", url: path + "2025.05.27_dnh.png" },
-    { value: "2025.05.10_2",   label: "2025.05.10_2",       url: path + "2025.05.10_2.png" },
-    { value: "2025.05.10",     label: "2025.05.10",         url: path + "2025.05.10.png" },
-    { value: "2025.05.04",     label: "2025.05.04",         url: path + "2025.05.04.png" },
-    { value: "2025.05.03",     label: "2025.05.03",         url: path + "2025.05.03.jpg" }
+    { value: "2025.08.09_klb",  label: "2025.08.09 Кулбоб", url: path + "2025.08.09_klb.png" },
+    { value: "2025.07.31_dnh",  label: "2025.07.31 Динахон", url: path + "2025.07.31_dnh.png" },
+    { value: "2025.07.04_dnh",  label: "2025.07.04 Динахон", url: path + "2025.07.04_dnh.png" },
+    { value: "2025.07.04_klb",  label: "2025.07.04 Кулбоб",  url: path + "2025.07.04_klb.png" },
+    { value: "2025.06.05_dnh",  label: "2025.06.05 Динахон", url: path + "2025.06.05_dnh.png" },
+    { value: "2025.05.27_klb",  label: "2025.05.27 Кулбоб",  url: path + "2025.05.27_klb.png" },
+    { value: "2025.05.27_dnh",  label: "2025.05.27 Динахон", url: path + "2025.05.27_dnh.png" },
+    { value: "2025.05.10_2_klb",label: "2025.05.10_2 Кулбоб",url: path + "2025.05.10_2_klb.png" },
+    { value: "2025.05.10_klb",  label: "2025.05.10 Кулбоб",  url: path + "2025.05.10_klb.png" },
+    { value: "2025.05.04_klb",  label: "2025.05.04 Кулбоб",  url: path + "2025.05.04_klb.png" },
+    { value: "2025.05.03_klb",  label: "2025.05.03 Кулбоб",  url: path + "2025.05.03_klb.jpg" }
 ];
 
 export default s5
