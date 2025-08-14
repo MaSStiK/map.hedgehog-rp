76527c30be6dd2cdfd3908d4b05a73c4a3ddaf28
hedgedan2805 <danieland032008@gmail.com>
2025-08-14 20:20:48 +0500

    season1

--- src/components/MapSelection/s1.js
+++ src/components/MapSelection/s1.js
@@ -3,14 +3,27 @@ const path = "/maps/s1/"
 const s1 = [
     { value: "physical-map",    label: "Физическая карта",               url: path + "physical-map.png" },
     { value: "space-map",       label: "Космическая карта",              url: path + "space-map.png" },
+    { value: "2021.06.04",      label: "2021.06.04",        url: path + "2021.06.04.png" },
+    { value: "2021.04.15",      label: "2021.04.15",        url: path + "2021.04.15.png" },
+    { value: "2021.03.26",      label: "2021.03.26",        url: path + "2021.03.26.png" },
     { value: "2021.03.25",      label: "2021.03.25 Великоежинск",        url: path + "2021.03.25.png" },
     { value: "2021.03.21",      label: "2021.03.21 Коалоежинск",         url: path + "2021.03.21.png" },
     { value: "2021.03.14",      label: "2021.03.14 Афины",               url: path + "2021.03.14.png" },
+    { value: "2021.01.30",      label: "2021.01.30",   url: path + "2021.01.30.png" },
+    { value: "2020.12.28",      label: "2020.12.28",   url: path + "2020.12.28.png" },
+    { value: "2020.12.20",      label: "2020.12.20",   url: path + "2020.12.20.png" },
     { value: "2020.12.XX",      label: "2020.12.XX Цанг",                url: path + "2020.12.XX.png" },
+    { value: "2020.12.13",      label: "2020.12.13",   url: path + "2020.12.13.png" },
     { value: "2020.11.30",      label: "2020.11.30 Лос-Анджелес",        url: path + "2020.11.30.png" },
     { value: "2020.11.15",      label: "2020.11.15 Санса",               url: path + "2020.11.15.png" },
-    { value: "2020.10.02_2",    label: "2020.10.02 Великоежиное Гос. 2", url: path + "2020.10.02_2.png" },
-    { value: "2020.10.02",      label: "2020.10.02 Великоежиное Гос.",   url: path + "2020.10.02.png" }
+    { value: "2020.10.18",      label: "2020.10.18",   url: path + "2020.10.18.png" },
+    { value: "2020.10.02_2",    label: "2020.10.02_2 Великоежиное Гос. 2", url: path + "2020.10.02_2.png" },
+    { value: "2020.10.02",      label: "2020.10.02 Великоежиное Гос.",   url: path + "2020.10.02.png" },
+    { value: "2020.09.19",      label: "2020.09.19",   url: path + "2020.09.19.png" },
+    { value: "2020.08.12",      label: "2020.08.12",   url: path + "2020.08.12.png" },
+    { value: "2020.07.04",      label: "2020.07.04",   url: path + "2020.07.04.png" },
+    { value: "2020.06.06",      label: "2020.06.06",   url: path + "2020.06.06.png" },
+    { value: "2020.05.20",      label: "2020.05.20",   url: path + "2020.05.20.png" },
 ];
 
 export default s1
