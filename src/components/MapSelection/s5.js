8f5dd3ae92a4535997f004f1a51fbf34106d1614
hedgedan2805 <danieland032008@gmail.com>
2025-08-14 19:45:19 +0500

    2025.08.14

--- src/components/MapSelection/s5.js
+++ src/components/MapSelection/s5.js
@@ -1,7 +1,10 @@
 const path = "/maps/s5/"
 
 const s5 = [
-    { value: "2025.08.09_klb",  label: "2025.08.09 Кулбоб", url: path + "2025.08.09_klb.png" },
+    { value: "2025.08.14_klb",  label: "2025.08.14 Кулбоб", url: path + "2025.14.03_klb.png" },
+    { value: "2025.08.13_klb",  label: "2025.08.13 Кулбоб", url: path + "2025.13.03_klb.png" },
+    { value: "2025.08.12_klb",  label: "2025.08.12 Кулбоб", url: path + "2025.12.03_klb.png" },
+    { value: "2025.08.09_klb",  label: "2025.08.09 Кулбоб",  url: path + "2025.08.09_klb.png" },
     { value: "2025.07.31_dnh",  label: "2025.07.31 Динахон", url: path + "2025.07.31_dnh.png" },
     { value: "2025.07.04_dnh",  label: "2025.07.04 Динахон", url: path + "2025.07.04_dnh.png" },
     { value: "2025.07.04_klb",  label: "2025.07.04 Кулбоб",  url: path + "2025.07.04_klb.png" },
@@ -11,7 +14,8 @@ const s5 = [
     { value: "2025.05.10_2_klb",label: "2025.05.10_2 Кулбоб",url: path + "2025.05.10_2_klb.png" },
     { value: "2025.05.10_klb",  label: "2025.05.10 Кулбоб",  url: path + "2025.05.10_klb.png" },
     { value: "2025.05.04_klb",  label: "2025.05.04 Кулбоб",  url: path + "2025.05.04_klb.png" },
-    { value: "2025.05.03_klb",  label: "2025.05.03 Кулбоб",  url: path + "2025.05.03_klb.jpg" }
+    { value: "2025.05.03_klb",  label: "2025.05.03 Кулбоб",  url: path + "2025.05.03_klb.jpg" },
+ 
 ];
 
 export default s5
