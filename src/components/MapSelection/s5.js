26fa61201bf32ef0086353dce136ae4cfdad9ecc
hedgedan2805 <danieland032008@gmail.com>
2025-08-22 00:26:57 +0500

    zhh

--- src/components/MapSelection/s5.js
+++ src/components/MapSelection/s5.js
@@ -1,22 +1,21 @@
 const path = "/maps/s5/"
 
 const s5 = [
-    { value: "2025.08.15_klb",  label: "2025.08.15 Кулбоб", url: path + "2025.08.15_klb.png" },
-    { value: "2025.08.14_klb",  label: "2025.08.14 Кулбоб", url: path + "2025.08.14_klb.png" },
-    { value: "2025.08.13_klb",  label: "2025.08.13 Кулбоб", url: path + "2025.08.13_klb.png" },
-    { value: "2025.08.12_klb",  label: "2025.08.12 Кулбоб", url: path + "2025.08.12_klb.png" },
-    { value: "2025.08.09_klb",  label: "2025.08.09 Кулбоб",  url: path + "2025.08.09_klb.png" },
-    { value: "2025.07.31_dnh",  label: "2025.07.31 Динахон", url: path + "2025.07.31_dnh.png" },
-    { value: "2025.07.04_dnh",  label: "2025.07.04 Динахон", url: path + "2025.07.04_dnh.png" },
-    { value: "2025.07.04_klb",  label: "2025.07.04 Кулбоб",  url: path + "2025.07.04_klb.png" },
-    { value: "2025.06.05_dnh",  label: "2025.06.05 Динахон", url: path + "2025.06.05_dnh.png" },
-    { value: "2025.05.27_klb",  label: "2025.05.27 Кулбоб",  url: path + "2025.05.27_klb.png" },
-    { value: "2025.05.27_dnh",  label: "2025.05.27 Динахон", url: path + "2025.05.27_dnh.png" },
-    { value: "2025.05.10_2_klb",label: "2025.05.10_2 Кулбоб",url: path + "2025.05.10_2_klb.png" },
-    { value: "2025.05.10_klb",  label: "2025.05.10 Кулбоб",  url: path + "2025.05.10_klb.png" },
-    { value: "2025.05.04_klb",  label: "2025.05.04 Кулбоб",  url: path + "2025.05.04_klb.png" },
-    { value: "2025.05.03_klb",  label: "2025.05.03 Кулбоб",  url: path + "2025.05.03_klb.jpg" },
- 
+    { value: path + "2025.08.15_klb.png",   label: "2025.08.15 Кулбоб" },
+    { value: path + "2025.08.14_klb.png",   label: "2025.08.14 Кулбоб" },
+    { value: path + "2025.08.13_klb.png",   label: "2025.08.13 Кулбоб" },
+    { value: path + "2025.08.12_klb.png",   label: "2025.08.12 Кулбоб" },
+    { value: path + "2025.08.09_klb.png",   label: "2025.08.09 Кулбоб" },
+    { value: path + "2025.07.31_dnh.png",   label: "2025.07.31 Динахон" },
+    { value: path + "2025.07.04_dnh.png",   label: "2025.07.04 Динахон" },
+    { value: path + "2025.07.04_klb.png",   label: "2025.07.04 Кулбоб" },
+    { value: path + "2025.06.05_dnh.png",   label: "2025.06.05 Динахон" },
+    { value: path + "2025.05.27_klb.png",   label: "2025.05.27 Кулбоб" },
+    { value: path + "2025.05.27_dnh.png",   label: "2025.05.27 Динахон" },
+    { value: path + "2025.05.10_2_klb.png", label: "2025.05.10_2 Кулбоб" },
+    { value: path + "2025.05.10_klb.png",   label: "2025.05.10 Кулбоб" },
+    { value: path + "2025.05.04_klb.png",   label: "2025.05.04 Кулбоб" },
+    { value: path + "2025.05.03_klb.jpg",   label: "2025.05.03 Кулбоб" }
 ];
 
 export default s5
