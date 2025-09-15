0a254742e2b331002ef25ecf44ba2fcde465d8ae
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-09-15 14:24:32 +0200

    s3 fix labels

--- src/components/MapSelection/s3.js
+++ src/components/MapSelection/s3.js
@@ -2,14 +2,14 @@ const path = "/maps/s3/"
 
 const s3 = [
     { value: path + "physical-map.png", label: "Физическая карта" },
-    { value: path + "2024.06.03.png",   label: "03.06.2024" },
-    { value: path + "2024.03.15.png",   label: "15.03.2024" },
-    { value: path + "2024.02.XX.png",   label: "XX.02.2024" },
-    { value: path + "2024.01.XX.png",   label: "XX.01.2024" },
-    { value: path + "2023.12.XX.png",   label: "XX.12.2023" },
-    { value: path + "2023.12.11.png",   label: "11.12.2023" },
-    { value: path + "2023.11.01.png",   label: "01.11.2023" },
-    { value: path + "2023.10.28.png",   label: "28.10.2023" }
+    { value: path + "2024.06.03.png",   label: "2024.06.03" },
+    { value: path + "2024.03.15.png",   label: "2024.03.15" },
+    { value: path + "2024.02.XX.png",   label: "2024.02.XX" },
+    { value: path + "2024.01.XX.png",   label: "2024.01.XX" },
+    { value: path + "2023.12.XX.png",   label: "2023.12.XX" },
+    { value: path + "2023.12.11.png",   label: "2023.12.11" },
+    { value: path + "2023.11.01.png",   label: "2023.11.01" },
+    { value: path + "2023.10.28.png",   label: "2023.10.28" }
 ];
 
 export default s3
